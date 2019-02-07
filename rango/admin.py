67c6468bc6721f31e9920a08c0b0fef12a23dd8a
Frazz3l <2315720p@student.gla.ac.uk>
2019-02-07 17:12:43 +0000

    Completed slug section and added exception for null slug and updated name

--- rango/admin.py
+++ rango/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 from rango.models import Category, Page
 
@@ -6,8 +7,11 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category', 'url')
     
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
+    
 #registered models below
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 
