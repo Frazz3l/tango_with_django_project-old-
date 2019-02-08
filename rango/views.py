c6f787daa10f7df79ff6160247f39a154a39afe5
Frazz3l <2315720p@student.gla.ac.uk>
2019-02-08 17:38:28 +0000

    Fixed add category outside login bug

--- rango/views.py
+++ rango/views.py
@@ -151,17 +151,15 @@ def show_category(request, category_name_slug):
 
 def index(request):
 	request.session.set_test_cookie()
-
-    category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
-    context_dict = {'categories': category_list,'pages':page_list}
-
+	category_list = Category.objects.order_by('-likes')[:5]
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list,'pages':page_list}
+	
 	visitor_cookie_handler(request)
 	context_dict['visits'] = request.session['visits']
 	
 	response = render(request, 'rango/index.html', context_dict)
-	
-    return response
+	return response
 
 
 def about(request):
